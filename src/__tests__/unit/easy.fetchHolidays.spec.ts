83dbd292bba39da81b59970840f21600cce6bd88
9yurilee <9yurilee@gmail.com>
2025-02-04 16:33:34 +0900

    test: fetchHolidays 테스트 코드 작성 완료

--- src/__tests__/unit/easy.fetchHolidays.spec.ts
+++ src/__tests__/unit/easy.fetchHolidays.spec.ts
@@ -1,9 +1,19 @@
 import { fetchHolidays } from '../../apis/fetchHolidays';
 
 describe('fetchHolidays', () => {
-  it('주어진 월의 공휴일만 반환한다', () => {});
+  it('주어진 월의 공휴일만 반환한다', () => {
+    expect(fetchHolidays(new Date('2024-12-19'))).toEqual({ '2024-12-25': '크리스마스' });
+  });
 
-  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {});
+  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {
+    expect(fetchHolidays(new Date('2024-04-10'))).toEqual({});
+  });
 
-  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {});
+  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {
+    expect(fetchHolidays(new Date('2024-09-07'))).toEqual({
+      '2024-09-16': '추석',
+      '2024-09-17': '추석',
+      '2024-09-18': '추석',
+    });
+  });
 });
