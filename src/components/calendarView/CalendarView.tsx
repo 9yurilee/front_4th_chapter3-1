f88b3fd3a12e2293ebc0fd29a022784048579cae
9yurilee <9yurilee@gmail.com>
2025-02-06 10:37:30 +0900

    refactor: 일정 달력 컴포넌트 분리

---
+++ src/components/calendarView/CalendarView.tsx
@@ -0,0 +1,41 @@
+import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
+import { Heading, HStack, IconButton, Select, VStack } from '@chakra-ui/react';
+
+import MonthView from './MonthView.tsx';
+import WeekView from './WeekView.tsx';
+import { useCalendarView } from '../../hooks/useCalendarView.ts';
+
+function CalendarView() {
+  const { view, setView, navigate } = useCalendarView();
+
+  return (
+    <VStack flex={1} spacing={5} align="stretch">
+      <Heading>일정 보기</Heading>
+
+      <HStack mx="auto" justifyContent="space-between">
+        <IconButton
+          aria-label="Previous"
+          icon={<ChevronLeftIcon />}
+          onClick={() => navigate('prev')}
+        />
+        <Select
+          aria-label="view"
+          value={view}
+          onChange={(e) => setView(e.target.value as 'week' | 'month')}
+        >
+          <option value="week">Week</option>
+          <option value="month">Month</option>
+        </Select>
+        <IconButton
+          aria-label="Next"
+          icon={<ChevronRightIcon />}
+          onClick={() => navigate('next')}
+        />
+      </HStack>
+
+      {view === 'week' ? <WeekView /> : <MonthView />}
+    </VStack>
+  );
+}
+
+export default CalendarView;
